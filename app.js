d8da97b66aec205ab064b4fd9ac16055aebf2e18
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2020-11-12 18:55:38 -0800

    axios requessts & container div created and connected

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+const addForm = document.querySelector('#searchBar');
 
+addForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const inputValue = addForm.querySelector('#game').value;
+  return inputValue
+  console.dir(inputValue.value)
+  fetchData(inputValue.value)
+  removeResults();
+});
 
-const getResults = async (game) => {
-  const url = `https://www.cheapshark.com/api/1.0/games?title=${game}&limit=60&exact=0`
-
+let dataContainer = document.querySelector('#game-data-collector')
+const getResults = async () => {
+  const url = `https://www.cheapshark.com/api/1.0/games?title=${game}&limit=60&exact=0`;
   try {
-    const response = await axios.get(url)
-    console.log(response.data)
+    let response = await axios.get(url)
+    showResGames(response.data)
 
   } catch (error) {
     console.log(`${error}`)
   }
+}
+
+function showResGames(data) {
+  console.log(data);
+  let gameInfo =
+    `<img src = ${data.thumb}></img>
+    <h1>${data.external}</h1>
+    <h3>${data.cheapest}</h3>`;
+  dataContainer.insertAdjacentHTML("beforeend", gameInfo);
 }
