a0b878b1a0426464dcac604e5d19f27f211843e8
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2020-11-16 02:09:03 -0800

    adjusted css and js files

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+//Global Variables
 let gotGameData;
+
 
 
 //select div to house the fetched data
@@ -8,8 +10,6 @@ const getResults = async (title) => {
   const url = `https://www.cheapshark.com/api/1.0/games?title=${title}&limit=60&exact=0`;
   try {
     let response = await axios.get(url)
-    // console.log(response.data)
-    // showResGames(response.data)
     appendData(response.data)
   } catch (error) {
     console.log(`${error}`)
@@ -25,6 +25,9 @@ function appendData(data) {
     let title = document.createElement('h2')
     title = game.external
     dataContainer.append(title)
+    let spaceBetween = document.createElement('p')
+    spaceBetween = ("--->")
+    dataContainer.append(spaceBetween)
     let price = document.createElement('h4')
     price = game.cheapest
     dataContainer.append(price)
@@ -47,18 +50,10 @@ function formData(event) {
   console.log(inputValue)
   getResults(inputValue)
   removeResults();
-  if (inputValue.value === 0) {
-    document.addTextContent("")
-  }
+
 }
 addForm.addEventListener('submit', formData)
-// addForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const inputValue = addForm.querySelector('#game').value;
-//   console.log(inputValue)
-//   // fetchData(inputValue.value)
-//   // removeResults();
-// });
+
 
 // remove previous search results
 removeResults = () => {
